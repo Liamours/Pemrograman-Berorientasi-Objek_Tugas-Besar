c8062896e7865a163b994dd3d3cbef923c6c07ee
rafbiii <rafiqlabib0515@gmail.com>
2025-05-09 19:35:22 +0700

    upload file BE

---
+++ backend/src/main/java/com/example/rest_service/repository/UserRepository.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.rest_service.repository;
+
+import com.example.rest_service.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
