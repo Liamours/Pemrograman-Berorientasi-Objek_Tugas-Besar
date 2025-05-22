452d93eea16763d0be983ca231be52c43be4bf8b
Fathan Arya Maulana <fathana.mrj951@gmail.com>
2025-05-22 09:14:21 +0700

    nambahin profile page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './LoginPage';
 import RegistrationPage from './RegistrationPage';
+import ProfilePage from './ProfilePage';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegistrationPage />} />
-        <Route path="*" element={<LoginPage />} />
+        <Route path="*" element={<ProfilePage />} />
       </Routes>
     </Router>
   );
