c55d9592d350a5f708fddeb236e29913a12678cd
THmmmHT <dzakyalfaris33@gmail.com>
2025-06-07 13:46:25 +0700

    user model fiks

--- backend/src/main/java/com/example/rest_service/model/User.java
+++ backend/src/main/java/com/example/rest_service/model/User.java
@@ -8,13 +8,14 @@ import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "users")
+@Table(name = "user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long id;
     
-    private String name;
+    private String nama_user;
     
     @Column(unique = true, nullable = false)
     private String email;
@@ -42,8 +43,8 @@ public class User {
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public String getName() { return nama_user; }
+    public void setName(String name) { this.nama_user = name; }
 
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
