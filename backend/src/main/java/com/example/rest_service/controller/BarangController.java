fb75aeb71111449d3edced3c9d59761fd9d1ed48
rafbiii <rafiqlabib0515@gmail.com>
2025-06-10 18:58:44 +0700

    succed fitur semua barang

---
+++ backend/src/main/java/com/example/rest_service/controller/BarangController.java
@@ -0,0 +1,60 @@
+package com.example.rest_service.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import com.example.rest_service.model.Barang;
+import com.example.rest_service.service.BarangService;
+import com.example.rest_service.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.rest_service.model.User;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/barang")
+public class BarangController {
+
+    @Autowired
+    private BarangService barangService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/product")
+    public ResponseEntity<List<Barang>> getAllBarang() {
+        List<Barang> barangList = barangService.getAllBarang();
+        return ResponseEntity.ok(barangList);
+    }
+
+    @PostMapping("/product/new")
+    @PreAuthorize("hasRole('Admin')")
+    public ResponseEntity<String> createProduct(@RequestBody Barang product, Authentication authentication) {
+        String email = authentication.getName();
+        Optional<User> optionalUser = userRepository.findByEmail(email); // Mendapatkan user berdasarkan email
+
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();  // Ambil user dari Optional
+            if ("Admin".equals(user.getPeran())) {
+                barangService.addProduct(product);  // Proses penyimpanan barang
+                return ResponseEntity.ok("Product created successfully!");
+            } else {
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).body("You are not authorized to create a product.");
+            }
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found.");
+        }
+    }
+
+}
--- backend/src/main/java/com/example/rest_service/model/Order.java
+++ backend/src/main/java/com/example/rest_service/model/Order.java
@@ -97,4 +97,3 @@ public class Order {
         this.keranjang = keranjang;
     }
 }
-
