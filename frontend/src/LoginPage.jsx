cc29d7d7a356e0d75184922de81098f825028914
Fathan Arya Maulana <fathana.mrj951@gmail.com>
2025-06-10 14:27:24 +0700

    uodate FE

--- frontend/src/LoginPage.jsx
+++ frontend/src/LoginPage.jsx
@@ -27,8 +27,14 @@ function LoginPage() {
       const data = await response.json();
   
       if (response.ok) {
-        localStorage.setItem("token", data.data.token); 
-        navigate('/profile');
+        localStorage.setItem("token", data.data.token);
+        if (data.data.role == 'Admin') {
+          navigate('/admin/dashboard');
+          return;
+        } else if (data.role == 'Client') {
+          navigate('/profile');
+          return
+        }
       } else {
         setMessage(data.error || "Login gagal");
       }
