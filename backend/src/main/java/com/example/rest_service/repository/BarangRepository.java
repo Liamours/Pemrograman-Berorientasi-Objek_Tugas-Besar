d57c2986889e95a1607c25004c8d621cd4b165ef
rafbiii <rafiqlabib0515@gmail.com>
2025-06-14 17:55:21 +0700

    fixed barang

--- backend/src/main/java/com/example/rest_service/repository/BarangRepository.java
+++ backend/src/main/java/com/example/rest_service/repository/BarangRepository.java
@@ -2,7 +2,7 @@ package com.example.rest_service.repository;
 
 import com.example.rest_service.model.Barang;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
--- backend/src/main/java/com/example/rest_service/repository/DetailOrderRepository.java
+++ backend/src/main/java/com/example/rest_service/repository/DetailOrderRepository.java
@@ -2,6 +2,7 @@ package com.example.rest_service.repository;
 
 import com.example.rest_service.model.DetailOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 public interface DetailOrderRepository extends JpaRepository<DetailOrder, Integer> {
 }
