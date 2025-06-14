49709613afff31800961f40f3244441fed7faedb
Liamours <143605412+Liamours@users.noreply.github.com>
2025-06-14 23:59:07 +0700

    Menambahkan models

--- backend/src/main/java/com/example/rest_service/model/Order.java
+++ backend/src/main/java/com/example/rest_service/model/Order.java
@@ -1,100 +1,62 @@
-package com.example.rest_service.model;
+package com.masai.models;
 
-import jakarta.persistence.*;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 @Entity
-@Table(name = "`order`")
+@Table(name="orders")
 public class Order {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int orderId;
-
-    @ManyToOne
-    @JoinColumn(name = "keranjang_id", referencedColumnName = "id")  // Menggunakan kolom id dari Keranjang
-    private Keranjang keranjang;
-
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "barang_id", nullable = false)
-    private Barang barang;
-
-    private Integer jumlahBarang;
-    private Double hargaPerUnit;
-    private LocalDateTime tanggalOrder;
-    private String alamatTujuan;
-
-    @Column(name = "status_order")
-    @Enumerated(EnumType.STRING)
-    private StatusOrder statusOrder;
-
-    public enum StatusOrder {
-        Pending_Client,  // Pastikan nama sesuai
-        Pending_Admin,
-        Done
-    }
-
-    // Getters and setters
-    public Integer getOrderId() {
-        return orderId;
-    }
-
-    public void setOrderId(Integer orderId) {
-        this.orderId = orderId;
-    }
-
-    public Barang getBarang() {
-        return barang;
-    }
-
-    public void setBarang(Barang barang) {
-        this.barang = barang;
-    }
-
-    public Integer getJumlahBarang() {
-        return jumlahBarang;
-    }
-
-    public void setJumlahBarang(Integer jumlahBarang) {
-        this.jumlahBarang = jumlahBarang;
-    }
-
-    public Double getHargaPerUnit() {
-        return hargaPerUnit;
-    }
-
-    public void setHargaPerUnit(Double hargaPerUnit) {
-        this.hargaPerUnit = hargaPerUnit;
-    }
-
-    public LocalDateTime getTanggalOrder() {
-        return tanggalOrder;
-    }
-
-    public void setTanggalOrder(LocalDateTime tanggalOrder) {
-        this.tanggalOrder = tanggalOrder;
-    }
-
-    public String getAlamatTujuan() {
-        return alamatTujuan;
-    }
-
-    public void setAlamatTujuan(String alamatTujuan) {
-        this.alamatTujuan = alamatTujuan;
-    }
-
-    public StatusOrder getStatusOrder() {
-        return statusOrder;
-    }
-
-    public void setStatusOrder(StatusOrder statusOrder) {
-        this.statusOrder = statusOrder;
-    }
-
-    public Keranjang getKeranjang() {
-        return keranjang;
-    }
-
-    public void setKeranjang(Keranjang keranjang) {
-        this.keranjang = keranjang;
-    }
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer orderId;
+	@PastOrPresent
+	private LocalDate date;
+	@NotNull
+	@Enumerated(EnumType.STRING)
+	private OrderStatusValues orderStatus;
+	
+	private Double total;
+	
+	private String cardNumber;
+	
+	@JsonIgnore
+	@ManyToOne
+	@JoinColumn(name = "customer_id", referencedColumnName = "customerId")
+	private Customer customer;
+	
+	@OneToMany
+	private List<CartItem> ordercartItems = new ArrayList<>();
+	
+	@ManyToOne
+	@JoinColumn(name = "address_id", referencedColumnName = "addressId")
+	private Address address;
 }
-
