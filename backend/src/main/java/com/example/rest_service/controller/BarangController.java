8921f6f3fcc89dd342f5d5f404f031b0c6d7ca9c
Fathan Arya Maulana <fathana.mrj951@gmail.com>
2025-06-15 14:18:05 +0700

    update

--- backend/src/main/java/com/example/rest_service/controller/BarangController.java
+++ backend/src/main/java/com/example/rest_service/controller/BarangController.java
@@ -11,7 +11,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -37,7 +36,7 @@ public class BarangController {
     @Autowired
     private BarangService barangService;
 
-    @GetMapping
+    @PostMapping
     public ResponseEntity<ApiResponse> getAllBarang(@RequestBody BarangFilterRequest filterRequest) {
         try {
             List<Barang> filteredBarang = barangService.getFilteredBarang(filterRequest);
