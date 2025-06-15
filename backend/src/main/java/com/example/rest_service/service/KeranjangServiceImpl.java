7f599d6e8b7acd2032a9e0795e76ef347a36c3a0
Fathan Arya Maulana <fathana.mrj951@gmail.com>
2025-06-15 09:31:26 +0700

    update

--- backend/src/main/java/com/example/rest_service/service/KeranjangServiceImpl.java
+++ backend/src/main/java/com/example/rest_service/service/KeranjangServiceImpl.java
@@ -1,11 +1,11 @@
-package com.example.rest_service.service.impl;
+package com.example.rest_service.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.rest_service.model.Keranjang;
 import com.example.rest_service.model.User;
 import com.example.rest_service.repository.KeranjangRepository;
-import com.example.rest_service.service.KeranjangService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 @Service
 public class KeranjangServiceImpl implements KeranjangService {
