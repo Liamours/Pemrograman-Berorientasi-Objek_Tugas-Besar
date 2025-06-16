0d263ff9f15b153fb153749238aa67e04a95eec7
rafbiii <rafiqlabib0515@gmail.com>
2025-06-16 08:02:11 +0700

    update inheritance

--- backend/src/main/java/com/example/rest_service/repository/ClientRepository.java
+++ backend/src/main/java/com/example/rest_service/repository/ClientRepository.java
@@ -1,14 +1,24 @@
 package com.example.rest_service.repository;
 
 import com.example.rest_service.model.Client;
-import com.example.rest_service.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
-    Optional<Client> findByUser(User user);
-}
 
+    Optional<Client> findByEmail(String email);
+
+    @Query("SELECT c FROM Client c WHERE c.clientDetail.ismember = true")
+    List<Client> findAllMembers();
+
+    @Query("SELECT c FROM Client c WHERE c.clientDetail.ismember = false")
+    List<Client> findAllNonMembers();
+
+    @Query("SELECT c FROM Client c WHERE c.clientDetail.alamat LIKE %:alamat%")
+    List<Client> findByAlamatContaining(@Param("alamat") String alamat);
+}
